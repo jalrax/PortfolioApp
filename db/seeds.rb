f36ee9d6d3c1b607706ff9365877b21b384708f6
jalrax <trymental@gmail.com>
2020-05-25 19:41:07 +0300

    implemented show functionality && buttons

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,8 +17,8 @@ puts "10 blog posts created"
 
 5.times do |skill|
   Skill.create!(
-    title: "Rails #{skill}",
-    percent_utilized: 15
+      title: "Rails #{skill}",
+      percent_utilized: 15
   )
 end
 
