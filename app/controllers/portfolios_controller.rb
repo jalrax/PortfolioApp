9aeb540d9da7ea8ae89b468e6fd0360a1ecb17ae
jalrax <trymental@gmail.com>
2020-07-03 19:26:52 +0300

    added guest user for current_user & refactored portfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+
   def index
     @portfolio_items = Portfolio.all
   end
@@ -7,13 +9,19 @@ class PortfoliosController < ApplicationController
     @angular_portfolio_items = Portfolio.angular
   end
 
+  def show
+  end
+
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
   end
 
+  def edit
+  end
+
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -24,14 +32,9 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  def edit
-    @portfolio_item = Portfolio.find(params[:id])
-  end
-
   def update
-    @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Record was successfully updated.' }
       else
         format.html { render :edit }
@@ -39,15 +42,24 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  def show
-    @portfolio_item = Portfolio.find(params[:id])
-  end
-
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
     @portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was successfully destroyed.' }
     end
+  end
+
+  private
+
+  def set_portfolio
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+    )
   end
 end
